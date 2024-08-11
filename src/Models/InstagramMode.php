49bc4a1bb3bdd54a640293375fdb0d3b933174e1
Hasan Yüksektepe <hasan.yuksektepe@ogr.iu.edu.tr>
2024-08-11 14:55:54 +0300

    2.0.32

--- src/Models/InstagramMode.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    namespace Hasokeyk\InstagramPro;
-
-    class Mode{
-        const MOBILE_WEP = "MOBILE_WEP";
-        const MOBILE_APP = "MOBILE_APP";
-    }
--- src/Models/InstagramUseType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    namespace Hasokeyk\InstagramPro;
-
-    class UseType{
-        const HACK = "HACK";
-        const NORMAL = "NORMAL";
-        const STALK = "STALK";
-    }
